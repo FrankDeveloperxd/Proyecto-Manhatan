85ab461935c866894f0b914e33ab530c93c167db
FrankDeveloperxd <bernaola.p.frank@gmail.com>
2025-10-24 08:03:01 -0500

    feat:sacando private seach

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 // src/main.tsx
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
-import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <AuthProvider>
     <App />
-    </AuthProvider>
 );
