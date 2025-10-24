8975f5863b4748477dd7df541768efde8389c23f
FrankDeveloperxd <bernaola.p.frank@gmail.com>
2025-10-24 08:35:30 -0500

    bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <HashRouter>
         <App />
-    </HashRouter>
 );
